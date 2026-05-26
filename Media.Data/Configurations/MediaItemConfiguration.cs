ca727a2950bb88bc2ecaa08f0fcb4dda372bde1b
cglock <cglock84@gmail.com>
2026-05-25 22:01:18 -0500

    updated MediaItemConfiguration file

---
+++ Media.Data/Configurations/MediaItemConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Media.Data.Entities;
+
+namespace Media.Data.Configurations;
+public class MediaItemConfigutration : IEntityTypeConfiguration<MediaItem> 
+{
+    public void Configure(EntityTypeBuilder<MediaItem> builder)
+    {
+        //Table Name
+        builder.ToTable("MediaItems");
+
+        //Primary Key
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(2000);
+
+        builder.Property(x => x.ThumbnailPath)
+            .HasMaxLength(500);
+
+        builder.Property(x => x.MediaType)
+            .IsRequired();
+        
+        builder.Property(x => x.CreatedDate)
+            .IsRequired();
+
+        //Indexes
+        builder.HasIndex(x => x.Title);
+
+        builder.HasIndex(x => x.MediaType);
+
+        builder.HasIndex(x => x.CreatedDate);
+
+        //Relationships
+
+        //MediaItem -> Library (Many-to-One)
+        builder.HasOne(x => x.Library)
+            .WithMany(x => x.MediaItems)
+            .HasForeignKey(x => x.LibraryId)
+            .OnDelete(DeleteBehaviour.Restrict);
+
+        builder.HasMany(x => x.MediaFiles)
+            .WithOne(x => x.MediaItem)
+            .HasForeignKey(x => x.MediaItemId)
+            .OnDelete(DeleteBehaviour.Cascade);
+
+    }
+}
